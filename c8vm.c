66867ab0d95915d427574db607029addeaaf1b22
Defendi <tiagodefendidasilva@gmail.com>
2025-10-28 17:14:11 -0300

    group 8 - logical shifts

--- c8vm.c
+++ c8vm.c
@@ -200,7 +200,7 @@ void VM_ExecutarInstrucao(VM* vm) {
             // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
             if (NN = 0x6) {
                 vm->V[0xF] = vm->V[X] & 0x01;
-                vm->V[X] /= 2;
+                vm->V[X] >>= 2;
                 break;
             }
 
@@ -223,7 +223,7 @@ void VM_ExecutarInstrucao(VM* vm) {
             // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
             if (NN = 0xE) {
                 vm->V[0xF] = vm->V[X] & 0x80;
-                vm->V[X] *= 2;
+                vm->V[X] <<= 1;
                 break;
             }
 
