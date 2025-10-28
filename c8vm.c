0612984a38d6832ea0129d76b7342587ac0fc488
Defendi <tiagodefendidasilva@gmail.com>
2025-10-28 20:38:21 -0300

    group F - LD

--- c8vm.c
+++ c8vm.c
@@ -339,6 +339,13 @@ void VM_ExecutarInstrucao(VM* vm) {
             }
 
             // LD B, Vx
+            // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+            // The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I,
+            // the tens digit at location I+1, and the ones digit at location I+2.
+            if (NN = 0x33) {
+                // TODO: MEMORY
+            }
+
             // LD [I], Vx
             // LD Vx, [I]
             break;
